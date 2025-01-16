30072d65f4fcf19201ff2e3219c2916adcac76c7
Gleb <gleb.gleb.tula@mail.ru>
2025-01-16 17:04:01 +0300

    start auth service

---
+++ core/services/auth/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import schemas
+from .database import SessionLocal
+from .models.user import User
+import auth
+app = FastAPI()
+
+DATABASE_URL = "sqlite:///db.sqlite3"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/register")
+def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    db_user = db.query(models.User).filter(models.User.username == user.username).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    
+    hashed_password = auth.create_hashed_password(user.password)
+    db_user = models.User(username=user.username, hashed_password=hashed_password)
+    
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    
+    return {
+        "id": db_user.id,
+        "username": db_user.username
+    }
