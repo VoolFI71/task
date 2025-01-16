e62f64661b56ba4b0247dba52b11ae889e655192
Gleb <gleb.gleb.tula@mail.ru>
2025-01-16 16:31:34 +0300

    +balance to model user

--- core/models/base.py
+++ core/models/base.py
@@ -3,11 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
 
-# Создаем базовый класс для моделей
 class Base(DeclarativeBase):
     __abstract__ = True
-    id: Mapped[int] = mapped_column(primary_key=True)
 
-    @declared_attr.directive
+    @declared_attr
+    def id(cls) -> Mapped[int]:
+        return mapped_column(Integer, primary_key=True)
+
+    @declared_attr
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}s"
--- core/models/user.py
+++ core/models/user.py
@@ -1,12 +1,11 @@
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
-
 from .base import Base
 
 class User(Base):
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    hash_password: Mapped[str] = mapped_column(String, nullable=False)
+    balance: Mapped[int] = mapped_column(Integer, default=0) 
